6f812984381f2e003d80acb7125b923ad9176974
josdic1 <joshuadicker@gmail.com>
2026-01-15 10:38:18 -0500

    deploy frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import {  routes } from './routes'
+import { routes } from './routes'
 import './index.css';
 
-
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes, {
+  basename: '/suddsy-demo/'
+})
 
 const root = createRoot(document.getElementById('root'))
-root.render(  <RouterProvider router={router} />)
-
+root.render(<RouterProvider router={router} />)
